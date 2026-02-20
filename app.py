9d01eef999ab4c11c40566da715f1af62ea1fbe9
EmilioArzo <253627026+IDGS-802-23002199@users.noreply.github.com>
2026-02-19 18:48:13 -0600

    modificar 19/02/2026

--- app.py
+++ app.py
@@ -17,9 +17,53 @@ csrf=CSRFProtect()
 def index():
     return render_template("index.html")
 
-@app.route("/Alumnos")
+@app.route("/Alumnos",methods=['GET','POST'])
 def alumnos():
-    return render_template("alumnos.html")
+    create_from=forms.UserForm2(request.form)
+    if request.method=='POST':
+        alum=Alumnos(nombre=create_from.nombre.data, 
+                    apaterno=create_from.apaterno.data,
+                    email=create_from.email.data)
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("alumnos.html",form=create_from)
+
+
+@app.route("/detalles",methods=['GET','POST'])
+def detalles():
+    create_from=forms.UserForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        id=request.args.get('id')
+        nombre=alum1.nombre
+        apaterno=alum1.apaterno
+        email=alum1.email
+        
+    return render_template("detalles.html",nombre=nombre,apaterno=apaterno,email=email)
+
+@app.route("/modificar",methods=['GET','POST'])
+def modificar():
+    create_from=forms.UserForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_from.id.data=request.args.get('id')
+        create_from.id.data=alum1.nombre
+        create_from.id.data=alum1.apaterno
+        create_from.id.data=alum1.email
+    if request.method=='POST':
+        id=create_from.id.data
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum1.id=request.args.get('id')
+        alum1.nombre=alum1.nombre
+        alum1.apaterno=alum1.apaterno
+        alum1.email=alum1.email
+        db.session.add(alum1)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("alumnos.html",form=create_from)
 
 @app.errorhandler("404")
 def page_not_fount(e):
